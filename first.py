66a3300f1041583a9de636bdb9a89e39022fa62b
Andrew Stringfellow <abreezemail@gmail.com>
2023-04-03 10:38:24 -0400

    added hardware data

--- first.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-data = pd.read_csv("./data/breast-cancer.data")
-plt.rcParams["figure.figsize"] = [7.00,3.50]
-plt.rcParams["figure.autolayout"] = True
-print(data)
