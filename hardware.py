c00b8ad8bc5328a65ddd6486a80503b3b699d3c9
Andrew Stringfellow <abreezemail@gmail.com>
2023-04-03 10:51:10 -0400

    updated with hardware...started hardware.py file

--- hardware.py
+++ hardware.py
@@ -4,24 +4,25 @@ import pandas as pd
 from pandas import DataFrame as df
 from sklearn.decomposition import PCA
 
-data = pd.read_csv("./data/breast-cancer.data")
-dataframe = df(data=data)
+data_file = pd.read_csv("./hardware-data/machine.data")
+data = df(data=data_file)
 column_names = [
-    "Class",
-    "Age",
-    "Menopause",
-    "Tumor-Size",
-    "Inv-Nodes",
-    "Node-Caps",
-    "Deg-Malig",
-    "Breast",
-    "Breast-Quad",
-    "Irradiat"
+    "Vendor-Name",
+    "Model-Name",
+    "MYCT",
+    "MMIN",
+    "MMAX",
+    "CACH",
+    "CHMIN",
+    "CHMAX",
+    "PRP",
+    "ERP",
 ]
-dataframe.set_axis(column_names,axis=1,inplace=True)
+data.set_axis(column_names,axis=1,inplace=True)
 plt.rcParams["figure.figsize"] = [7.00,3.50]
 plt.rcParams["figure.autolayout"] = True
-# print(data)
+print(data)
 
-pca = PCA(n_components=2)
-pca.fit([dataframe["Age"],dataframe["Tumor-Size"]])
+pca = PCA(n_components=3)
+pca.fit(data[["MYCT","MMIN","MMAX","CACH","CHMIN","CHMAX","PRP"]])
+# pca.fit(data["MYCT"],data["MMIN"],data["MMAX"],)
