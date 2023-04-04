c35c7015e6bcb0572240355b2e6589ea79f26536
andrewstring <abreezemail@gmail.com>
2023-04-04 11:55:13 -0400

    updated car

--- car.py
+++ car.py
@@ -49,32 +49,12 @@ data = data[
 
 plt.rcParams["figure.figsize"] = [7.00,3.50]
 plt.rcParams["figure.autolayout"] = True
-print(data.to_string())
-# # print(data)
 
-# pca = PCA(n_components=7)
-# pca.fit(data[["MYCT","MMIN","MMAX","CACH","CHMIN","CHMAX","PRP"]])
-# print(pca.explained_variance_ratio_)
-# print(pca.singular_values_)
-# city_mpg = data["City-MPG"]
-# highway_mpg = data["Highway-MPG"]
-# horsepower = data["Horsepower"]
-# combined_cols = [[elem[0],elem[1],elem[2]] for elem in [city_mpg,highway_mpg,horsepower]]
-# combined_cols = [[int(entry[0]),int(entry[1]),int(entry[2])] for entry in np.dstack((city_mpg,highway_mpg,horsepower))]
-# pre_combined_cols = np.dstack((city_mpg,highway_mpg,horsepower))
+print(data[["CityMPG","HighwayMPG","PeakRPM"]])
 
-# for entry in combined_cols:
-#     print(entry)
 
-# print(combined_cols)
-
-# selected_cols = data[["City-MPG","Highway-MPG","Horsepower"]]
-# string_data = selected_cols.to_string()
-# print(string_data)
-
-# plt.scatter(data["City-MPG"],data["Highway-MPG"],data["Horsepower"])
-# plt.scatter(selected_cols[0],selected_cols[1])
-# plt.show()
+plt.scatter(data["CityMPG"],data["HighwayMPG"],data["HighwayMPG"].tostring())
+plt.show()
 
 # plt.scatter(combined_cols[0],combined_cols[1],combined_cols[2])
 # plt.scatter(pre_combined_cols[0],pre_combined_cols[])
