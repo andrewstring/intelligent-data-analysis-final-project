a0ef98f5bd576920ea715ff2ed2d990b880f3165
andrewstring <abreezemail@gmail.com>
2023-04-04 11:34:12 -0400

    modified to match pandas

--- car.py
+++ car.py
@@ -43,19 +43,23 @@ plt.rcParams["figure.autolayout"] = True
 # pca.fit(data[["MYCT","MMIN","MMAX","CACH","CHMIN","CHMAX","PRP"]])
 # print(pca.explained_variance_ratio_)
 # print(pca.singular_values_)
-city_mpg = data["City-MPG"]
-highway_mpg = data["Highway-MPG"]
-horsepower = data["Horsepower"]
+# city_mpg = data["City-MPG"]
+# highway_mpg = data["Highway-MPG"]
+# horsepower = data["Horsepower"]
 # combined_cols = [[elem[0],elem[1],elem[2]] for elem in [city_mpg,highway_mpg,horsepower]]
 # combined_cols = [[int(entry[0]),int(entry[1]),int(entry[2])] for entry in np.dstack((city_mpg,highway_mpg,horsepower))]
-pre_combined_cols = np.dstack((city_mpg,highway_mpg,horsepower))
+# pre_combined_cols = np.dstack((city_mpg,highway_mpg,horsepower))
 
 # for entry in combined_cols:
 #     print(entry)
 
 # print(combined_cols)
 
+selected_cols = data[["City-MPG","Highway-MPG","Horsepower"]]
+print(selected_cols.head())
+# for row in selected_cols:
+#     print(row)
 
 # plt.scatter(combined_cols[0],combined_cols[1],combined_cols[2])
-plt.scatter(pre_combined_cols[0],pre_combined_cols[1])
-plt.show()
+# plt.scatter(pre_combined_cols[0],pre_combined_cols[])
+# plt.show()
