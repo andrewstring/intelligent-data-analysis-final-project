c47c89ce02a1db4a7b93243572fc6246858a5023
Andrew Stringfellow <abreezemail@gmail.com>
2023-04-06 23:15:05 -0400

    started flare file

---
+++ flare.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pandas import DataFrame as df
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import os.path
+
+
+
+
+
+
+def reorder_data(in_file_path,out_file_path):
+    if not os.path.isfile(out_file_path):
+        with open(out_file_path, "w+") as write:
+            with open(in_file_path, "r") as read:
+                file_lines = read.readlines()
+                for line in file_lines:
+                    if not line[0] == "*":
+                        write.write(line.replace(" ",","))
+
+
+reorder_data("./flare-data/flare.data1","./flare-data/newflare.data1")
+
+df = pd.read_csv("./flare-data/newflare.data1",header=None)
+
+print(df.head())
+df.set_axis(["0","1","2","3","4","5","6","7","8","9","10","11","12"],axis=1,inplace=False)
+print(df.head())
+
+plt.plot(df[["3","4"]])
+plt.show()
--- hardware.py
+++ hardware.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame as df
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 data_file = pd.read_csv("./hardware-data/machine.data")
 data = df(data=data_file)
@@ -19,11 +20,19 @@ column_names = [
     "ERP",
 ]
 data.set_axis(column_names,axis=1,inplace=True)
-plt.rcParams["figure.figsize"] = [7.00,3.50]
-plt.rcParams["figure.autolayout"] = True
-# print(data)
 
-pca = PCA(n_components=7)
-pca.fit(data[["MYCT","MMIN","MMAX","CACH","CHMIN","CHMAX","PRP"]])
-print(pca.explained_variance_ratio_)
-print(pca.singular_values_)
+# scaled_data = StandardScaler().fit_transform(data)
+scaled_data = StandardScaler().fit_transform(data[["MYCT","MMIN","MMAX","CACH","CHMIN","CHMAX","PRP"]])
+# print(scaled_data)
+pca = PCA(n_components=2)
+pca.fit(scaled_data)
+print(pca.components_)
+
+
+# plt.rcParams["figure.figsize"] = [7.00,3.50]
+# plt.rcParams["figure.autolayout"] = True
+# # print(data)
+
+# pca = PCA(n_components=2)
+# pca.fit(data[["MYCT","MMIN","MMAX","CACH","CHMIN","CHMAX","PRP"]])
+# print(pca.components_)
